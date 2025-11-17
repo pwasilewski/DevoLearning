1ef9a2cb4d0951e6acab601278faba59c6c68182
Piotr Wasilewski <pwasilewski93@proton.me>
2025-11-17 13:26:09 +0100

    feat: Exercise 06 (#6)
    Co-authored-by: Wasilewski Piotr <Piotr.Wasilewski@riziv-inami.fgov.be>

---
+++ solutions/06_PersonDetails/Features/Persons/Details/Models/PersonDetailsModel.cs
@@ -0,0 +1,70 @@
+﻿namespace Nihdi.DevoLearning.Presentation.Features.Persons.Details.Models
+{
+    public class PersonDetailsModel
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string LastName
+        {
+            get; set;
+        }
+
+        public string FirstName
+        {
+            get; set;
+        }
+
+        public int Gender
+        {
+            get; set;
+        }
+
+        public int CivilState
+        {
+            get; set;
+        }
+
+        public DateTime? BirthDate
+        {
+            get; set;
+        }
+
+        public DateTime? DeceasedDate
+        {
+            get; set;
+        }
+
+        public string Email
+        {
+            get; set;
+        }
+
+        public string Mobile
+        {
+            get; set;
+        }
+
+        public DateTime CreatedOn
+        {
+            get; set;
+        }
+
+        public string CreatedBy
+        {
+            get; set;
+        }
+
+        public DateTime ModifiedOn
+        {
+            get; set;
+        }
+
+        public string ModifiedBy
+        {
+            get; set;
+        }
+    }
+}
---
+++ solutions/06_PersonDetails/Features/Persons/Details/ViewModels/IPersonDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Nihdi.DevoLearning.Presentation.Features.Persons.Details.ViewModels
+{
+    using Nihdi.DevoLearning.Presentation.Features.Persons.Details.Models;
+    using Nihdi.DevoLearning.Presentation.Shared;
+
+    public interface IPersonDetailsViewModel
+    {
+        bool IsLoading
+        {
+            get;
+        }
+
+        PersonDetailsModel Person
+        {
+            get;
+        }
+
+        Task OnInitializedAsync(IErrorComponent errorComponent, int id);
+    }
+}
