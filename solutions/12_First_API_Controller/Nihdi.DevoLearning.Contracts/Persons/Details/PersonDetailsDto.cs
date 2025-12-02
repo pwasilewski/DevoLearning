19f8628630437648ca6df69503efab7d96235ad6
Piotr Wasilewski <pwasilewski93@proton.me>
2025-12-02 17:04:25 +0100

    feat: Exercise12 (#21)

---
+++ solutions/12_First_API_Controller/Nihdi.DevoLearning.Contracts/Persons/Details/PersonDetailsDto.cs
@@ -0,0 +1,70 @@
+﻿namespace Nihdi.DevoLearning.Contracts.Persons.Details
+{
+    public class PersonDetailsDto
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string LastName
+        {
+            get; set;
+        }
+
+        public string FirstName
+        {
+            get; set;
+        }
+
+        public int GenderId
+        {
+            get; set;
+        }
+
+        public int CivilStateId
+        {
+            get; set;
+        }
+
+        public DateTime? BirthDate
+        {
+            get; set;
+        }
+
+        public DateTime? DeceasedDate
+        {
+            get; set;
+        }
+
+        public string Email
+        {
+            get; set;
+        }
+
+        public string Mobile
+        {
+            get; set;
+        }
+
+        public DateTime CreatedOn
+        {
+            get; set;
+        }
+
+        public string CreatedBy
+        {
+            get; set;
+        }
+
+        public DateTime ModifiedOn
+        {
+            get; set;
+        }
+
+        public string ModifiedBy
+        {
+            get; set;
+        }
+    }
+}
---
+++ solutions/12_First_API_Controller/Nihdi.DevoLearning.Host.Bff/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿namespace Nihdi.DevoLearning.Host.Bff.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Nihdi.DevoLearning.Contracts.Persons.Details;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetPersonDetailsById(int id)
+        {
+            var result = new PersonDetailsDto()
+            {
+                Id = id,
+                LastName = "Vermeer",
+                FirstName = "Alice",
+                BirthDate = new DateTime(1992, 4, 10),
+                DeceasedDate = null,
+                GenderId = 2,
+                CivilStateId = 1,
+                Email = "alice.vermeer@example.com",
+                Mobile = "+32 475 11 22 33",
+                CreatedOn = new DateTime(2020, 1, 15, 10, 30, 0),
+                CreatedBy = "Migration",
+                ModifiedOn = new DateTime(2020, 1, 15, 10, 30, 0),
+                ModifiedBy = "Migration",
+            };
+
+            return Ok(result);
+        }
+    }
+}
